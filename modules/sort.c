1b93395e5a679f459e1033ee1e293d9523a0a493
Nuv <150278355+NUVXD@users.noreply.github.com>
2025-10-07 22:47:41 +0200

    implemented basic module skeleton

--- modules/sort.c
+++ modules/sort.c
@@ -3,7 +3,17 @@
 #include <lauxlib.h>
 #include <lualib.h>
 
-int luaopen_sort( lua_State *L )
-{
-    return 0;
+static int l_pixelSort(lua_State *L) {
+    int a = 1;
+    lua_pushnumber(L, a);
+    return 1;
+}
+
+__declspec(dllexport) int luaopen_sort(lua_State *L) {
+    luaL_Reg funcs[] = {
+        {"pixelSort", l_pixelSort},
+        {NULL, NULL}
+    };
+    luaL_newlib(L, funcs);
+    return 1;
 }
